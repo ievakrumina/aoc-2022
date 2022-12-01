d8b719a17424fcec298f961d6fda21b27f9f6559
maza_eksistence <ievakrumina93@gmail.com>
2022-12-01 07:27:43 +0100

    Day01

--- src/main/kotlin/Day01.kt
+++ src/main/kotlin/Day01.kt
@@ -1,10 +1,20 @@
-class Day01() {
+class Day01 {
 
-    fun solutionPart1() {
-        // TODO
+    private fun transformInput(input: List<String>): List<Int> {
+        return input.foldIndexed(mutableListOf<Int>()) { index, acc, s ->
+            if (index == 0 || s == "") {
+               acc.add(0)
+            }
+            if (s != "") acc[acc.lastIndex] += s.toInt()
+            acc
+        }
     }
 
-    fun solutionPart2() {
-        // TODO
+    fun solutionPart1(input: List<String>): Int? {
+        return transformInput(input).maxOrNull()
+    }
+
+    fun solutionPart2(input: List<String>): Int? {
+        return transformInput(input).sortedDescending().take(3).sum()
     }
 }
--- src/test/kotlin/Day01Test.kt
+++ src/test/kotlin/Day01Test.kt
@@ -1,24 +1,29 @@
 import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
 
 internal class Day01Test {
 
     @Test
     fun testPart1Example() {
-
+        val res = Day01().solutionPart1(Resources.resourceAsList("Day01_Example.txt"))
+        assertEquals(24000, res)
     }
 
     @Test
     fun testPart1Solution() {
-
+        val res = Day01().solutionPart1(Resources.resourceAsList("Day01.txt"))
+        assertEquals(69912, res)
     }
 
     @Test
     fun testPart2Example() {
-
+        val res = Day01().solutionPart2(Resources.resourceAsList("Day01_Example.txt"))
+        assertEquals(45000, res)
     }
 
     @Test
     fun testPart2Solution() {
-
+        val res = Day01().solutionPart2(Resources.resourceAsList("Day01.txt"))
+        assertEquals(208180, res)
     }
 }
