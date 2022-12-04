30cdb5da55f5d3bea993727d7bbf0438d3ec7503
maza_eksistence <ievakrumina93@gmail.com>
2022-12-04 16:22:33 +0100

    Improve day 2 solution

--- src/main/kotlin/Day02.kt
+++ src/main/kotlin/Day02.kt
@@ -1,65 +1,37 @@
 class Day02 {
 
     fun solutionPart1(input: List<String>): Int {
-        var score = 0
-        input.forEach {
-            val res = it.trim().split(" ")
-            when (res[0]) {
-                "A" -> {
-                    when (res[1]) {
-                        "X" -> score += 1 + 3
-                        "Y" -> score += 2 + 6
-                        "Z" -> score += 3 + 0
-                    }
-                }
-                "B" -> {
-                    when (res[1]) {
-                        "X" -> score += 1 + 0
-                        "Y" -> score += 2 + 3
-                        "Z" -> score += 3 + 6
-                    }
-                }
-                "C" -> {
-                    when (res[1]) {
-                        "X" -> score += 1 + 6
-                        "Y" -> score += 2 + 0
-                        "Z" -> score += 3 + 3
-                    }
-                }
+        return input.map {
+            when (it) {
+                "A X" -> 1 + 3
+                "A Y" -> 2 + 6
+                "A Z" -> 3 + 0
+                "B X" -> 1 + 0
+                "B Y" -> 2 + 3
+                "B Z" -> 3 + 6
+                "C X" -> 1 + 6
+                "C Y" -> 2 + 0
+                "C Z" -> 3 + 3
+                else -> 0
             }
-        }
-        return score
+        }.sum()
     }
 
     fun solutionPart2(input: List<String>): Int {
-        var score = 0
-        input.forEach {
-            val res = it.trim().split(" ")
-            when (res[0]) {
-                "A" -> {
-                    when (res[1]) {
-                        "X" -> score += 0 + 3
-                        "Y" -> score += 3 + 1
-                        "Z" -> score += 6 + 2
-                    }
-                }
-                "B" -> {
-                    when (res[1]) {
-                        "X" -> score += 0 + 1
-                        "Y" -> score += 3 + 2
-                        "Z" -> score += 6 + 3
-                    }
-                }
-                "C" -> {
-                    when (res[1]) {
-                        "X" -> score += 0 + 2
 
-                        "Y" -> score += 3 + 3
-                        "Z" -> score += 6 + 1
-                    }
-                }
+        return input.map {
+            when (it) {
+                "A X" -> 0 + 3
+                "A Y" -> 3 + 1
+                "A Z" -> 6 + 2
+                "B X" -> 1 + 0
+                "B Y" -> 3 + 2
+                "B Z" -> 6 + 3
+                "C X" -> 0 + 2
+                "C Y" -> 3 + 3
+                "C Z" -> 6 + 1
+                else -> 0
             }
-        }
-        return score
+        }.sum()
     }
 }
