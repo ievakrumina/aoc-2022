8470fdb29c153e5b935c41345c9bd852374b7331
maza_eksistence <ievakrumina93@gmail.com>
2022-12-06 05:53:53 +0100

    Improve to use map instead of multiple for loops

--- src/main/kotlin/Day05.kt
+++ src/main/kotlin/Day05.kt
@@ -1,72 +1,55 @@
 class Day05 {
 
-    private fun createStackList(input: List<String>): MutableList<MutableList<String>> {
-        val crateStack = mutableListOf<MutableList<String>>()
-        val indexCrateList = input[input.indexOf("") - 1].trim().last()
-        for (i in 0 until indexCrateList.digitToInt()) {
-            crateStack.add(mutableListOf())
+    private fun createStackList(input: List<String>): MutableList<MutableList<Char>> {
+        val stackRows = input.takeWhile { it.contains('[') }
+        return (1..stackRows.last().length step 4).map { index ->
+            stackRows.mapNotNull { it.getOrNull(index) }
+                .filter { it.isUpperCase() }
+                .toMutableList()
+        }.toMutableList()
+    }
+
+    private fun getCrateCommand(
+        input: List<String>,
+        str: String = "",
+        i: Int
+    ): Triple<Int, Int, Int> {
+        val t = if (str == "") {
+            input[i].split(" ")
+        } else {
+            str.split(" ")
         }
-        for (i in 0..input.indexOf("") - 2) {
-            input[i]
-                .replace("    ", ".")
-                .replace("[", "")
-                .replace("]", "")
-                .replace(" ", "")
-                .toCharArray()
-                .forEachIndexed { index, s ->
-                    if (s != '.') {
-                        crateStack[index].add(s.toString())
-                    }
-                }
-        }
-        crateStack.map { it.reverse() }
-        return crateStack
+
+        val cratesToMove = t[1].toInt()
+        val fromCrate = t[3].toInt() - 1
+        val toCrate = t[5].toInt() - 1
+        return Triple(cratesToMove, fromCrate, toCrate)
     }
 
     fun solutionPart1(input: List<String>): String {
         val crateStack = createStackList(input)
-
-        for (i in input.indexOf("")+1 until input.size) {
-            val t = input[i]
-                .replace("move ", "")
-                .replace("from ", "")
-                .replace("to ","")
-                .split(" ")
-
-            val cratesToMove = t[0].trim().toInt()
-            val fromCrate = t[1].trim().toInt()-1
-            val toCrate = t[2].trim().toInt()-1
-
-            for (i in 0 until cratesToMove) {
-                if (crateStack[fromCrate].isNotEmpty() || crateStack[fromCrate].last() != "")
-                crateStack[toCrate].add(crateStack[fromCrate].last())
-                crateStack[fromCrate].removeLast()
-
+        input
+            .dropWhile { !it.startsWith("move") }
+            .map { str -> getCrateCommand(emptyList(), str, 0) }
+            .forEach { (toMove, from, to) ->
+                val cratesToMove = crateStack[from].take(toMove).reversed()
+                repeat(toMove) { crateStack[from].removeFirst() }
+                crateStack[to].addAll(0, cratesToMove)
             }
-
-        }
-        return crateStack.joinToString { it.last() }.replace(",","").replace(" ", "")
+            return crateStack.map { it.first() }.joinToString("")
     }
 
     fun solutionPart2(input: List<String>): String {
         val crateStack = createStackList(input)
-
-        for (i in input.indexOf("")+1 until input.size) {
-            val t = input[i]
-                .replace("move ", "")
-                .replace("from ", "")
-                .replace("to ","")
-                .split(" ")
-
-            val cratesToMove = t[0].trim().toInt()
-            val fromCrate = t[1].trim().toInt()-1
-            val toCrate = t[2].trim().toInt()-1
-
-            crateStack[toCrate].addAll(crateStack[fromCrate].subList(crateStack[fromCrate].size-cratesToMove, crateStack[fromCrate].size))
-            crateStack[fromCrate] = crateStack[fromCrate].subList(0,crateStack[fromCrate].size-cratesToMove)
-
-        }
-        return crateStack.joinToString { it.last() }.replace(",","").replace(" ", "")
+        input
+            .dropWhile { !it.startsWith("move") }
+            .map { str -> getCrateCommand(emptyList(), str, 0) }
+            .forEach { (toMove, from, to) ->
+                val cratesToMove = crateStack[from].take(toMove)
+                repeat(toMove) { crateStack[from].removeFirst() }
+                crateStack[to].addAll(0, cratesToMove)
+            }
+        return crateStack.joinToString { it.first().toString() }.replace(",", "").replace(" ", "")
 
     }
 }
